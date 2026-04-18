f469c998772d24bffcd29470560a7ef5c15a1959
Kyliann <kyliann.dognon@gmail.com>
2026-04-18 03:25:00 +0200

    debut de aura.h pour la nouvelle amelioration un peu plus complexe

---
+++ src/Aura.h
@@ -0,0 +1,24 @@
+#ifndef AURA_H
+#define AURA_H
+
+#include "Position.h"
+
+class Aura {
+private:
+    Position pos;
+    float rayon;
+    int degats;
+    float duree;
+    float intervalle;
+    float tempsDepuisCreation;
+    float tempsDepuisDernierDegat;
+
+public:
+    Aura(float x, float y, float r, int d, float dur, float inter);
+
+    Position getPosition() const;
+    float getRayon() const;
+    int getDegats() const;
+};
+
+#endif
